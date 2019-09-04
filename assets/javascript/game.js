9d1b3fc1ca01a8b577313294e1cec897ff67cf94
Jandy <reverdy@protonmail.com>
2019-09-03 19:25:48 -0700

    wins/loss counter working on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,116 +1,123 @@
-        $(document).ready(function () {
-                // console.log("working");
-                        // alert("remove later");
+$(document).ready(function () {
+    // console.log("working");
+    // alert("remove later");
 
-            // var imgArray = new Array();
+    // var imgArray = new Array();
 
-            // imgArray[0] = new Image();
-            // imgArray[0].src = '../../images/gem1.png';
+    // imgArray[0] = new Image();
+    // imgArray[0].src = '../../images/gem1.png';
 
-            // imgArray[1] = new Image();
-            // imgArray[1].src = '../images/gem2.png';  
+    // imgArray[1] = new Image();
+    // imgArray[1].src = '../images/gem2.png';  
 
-            // imgArray[2] = new Image();
-            // imgArray[2].src = '../images/gem3.png';
+    // imgArray[2] = new Image();
+    // imgArray[2].src = '../images/gem3.png';
 
-            // imgArray[3] = new Image();
-            // imgArray[3].src = '../images/gem4.png';
+    // imgArray[3] = new Image();
+    // imgArray[3].src = '../images/gem4.png';
 
-            // var imgArray = [gem1.png, gem2.png, gem3.jpg, gem4.png];
+    // var imgArray = [gem1.png, gem2.png, gem3.jpg, gem4.png];
 
-            // for (var i = 0; i < imgArray.length; i++) {
-                // var gemBtn = $("<button>");
-                // gemBtn.attr("class", "letter-button", "letter", "letter-button-color");
-                // gemBtn.attr("data-letter", imgArray[i]);
-                // gemBtn.text(imgArray[i]);
-                // document.createElement("IMG");
-            // }
-            var currentScore = 0;
-            var randomlyGeneratedNumber = getNum(19, 120);
-            var wins = 0;
-            var losses = 0;
+    // for (var i = 0; i < imgArray.length; i++) {
+    // var gemBtn = $("<button>");
+    // gemBtn.attr("class", "letter-button", "letter", "letter-button-color");
+    // gemBtn.attr("data-letter", imgArray[i]);
+    // gemBtn.text(imgArray[i]);
+    // document.createElement("IMG");
+    // }
+    var currentScore = 0;
+    var randomlyGeneratedNumber = getNum(19, 120);
+    var wins = 0;
+    var loss = 0;
 
-            // Declare a global variable that keeps track of gem values
-            var gemData = {
-                red: {
-                    value: 0
-                },
-                green: {
-                    value: 0
-                },
-                blue: {
-                    value: 0
-                },
-                purple: {
-                    value: 0
-                },
-            }
+    // Declare a global variable that keeps track of gem values
+    var gemData = {
+        red: {
+            value: 0
+        },
+        green: {
+            value: 0
+        },
+        blue: {
+            value: 0
+        },
+        purple: {
+            value: 0
+        },
+    }
 
-                
-              function getNum(min, max) {
-                return Math.floor(Math.random() * (max - min + 1) + min);   
-           }
 
-        // Declare variable named startGame and assigne a function
-            var startGame = function() {
-                // Assign new random value  to each gem color
-                gemData.red.value  = getNum(1, 12);
-                gemData.green.value  = getNum(1, 12);
-                gemData.blue.value  = getNum(1, 12);
-                gemData.purple.value  = getNum(1, 12);
-                // Add target value (randomly generated #) to page
-                $("#randomNum").text(randomlyGeneratedNumber);
-              
-            }
+    function getNum(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
-            var addValues = function(gemValue) {
-                // Create some logic to add score
-                currentScore += gemValue;
-                $("#numTotal").text(currentScore);
-                // Create some logic to evaluate score
-                    // If logic shows score is equal, they win and start game.
-                    if (currentScore === randomlyGeneratedNumber) {
-                        alert('Hey you won!')
-                        wins++;
+    // Declare variable named startGame and assigne a function
+    var startGame = function () {
+        // Assign new random value  to each gem color
+        gemData.red.value = getNum(1, 12);
+        gemData.green.value = getNum(1, 12);
+        gemData.blue.value = getNum(1, 12);
+        gemData.purple.value = getNum(1, 12);
+        // Add target value (randomly generated #) to page
+        $("#randomNum").text(randomlyGeneratedNumber);
 
-                        $("#wins").text(wins)
+    }
 
-                        startGame();
-                    }
-                        // Record win
-                    // If logic shows score is above, they lose and start game.
+    var addValues = function (gemValue) {
+        // Create some logic to add score
+        currentScore += gemValue;
+        $("#numTotal").text(currentScore);
+        // Create some logic to evaluate score
+        // If logic shows score is equal, they win and start game.
+        if (currentScore === randomlyGeneratedNumber) {
+            alert('Hey you won!');
+            wins++;
+            // Record win
+            $("#wins").text(wins);
+            // TODO: reset current #
+            startGame();
+        }
 
-                        // Record loss
-            }
+        // If logic shows score is above, they lose and start game.
+        if (currentScore > randomlyGeneratedNumber) {
+            alert('You lost - try again!');
+            loss++;
+            // Record loss
+            $("#losses").text(loss);
+            // TODO: reset current #
+            startGame();
+        }
 
-            $("#redGem").on("click", function() {
-                addValues(gemData.red.value);
-                console.log("You clicked a crystal, mofo!");
-              });
+    }
 
-              $("#greenGem").on("click", function() {
-                addValues(gemData.green.value);
-                console.log("Woot!");
-              });
+    $("#redGem").on("click", function () {
+        addValues(gemData.red.value);
+        console.log("You clicked a crystal, mofo!");
+    });
 
-              $("#blueGem").on("click", function() {
-                addValues(gemData.blue.value);
-                console.log("Finally!");
-              });
+    $("#greenGem").on("click", function () {
+        addValues(gemData.green.value);
+        console.log("Woot!");
+    });
 
-              $("#purpleGem").on("click", function() {
-                addValues(gemData.purple.value);
-                console.log("Gott seid dank!");
-              });
+    $("#blueGem").on("click", function () {
+        addValues(gemData.blue.value);
+        console.log("Finally!");
+    });
 
-              
-              
-//   $("button").click(function() { //when button clicked
-//  //  alert(this.id); // or alert($(this).attr('id'));
-//   var num=this.id; //storing id of button clicked in number variable
-//   var val=$("#"+num).val(); //storing button value into val variable 
-//   console.log(val); 
-//   $("#first-number").text(val);  //grabbing first div and displaying val 
-//   });
-              startGame();
-        })
+    $("#purpleGem").on("click", function () {
+        addValues(gemData.purple.value);
+        console.log("Gott seid dank!");
+    });
+
+
+
+    //   $("button").click(function() { //when button clicked
+    //  //  alert(this.id); // or alert($(this).attr('id'));
+    //   var num=this.id; //storing id of button clicked in number variable
+    //   var val=$("#"+num).val(); //storing button value into val variable 
+    //   console.log(val); 
+    //   $("#first-number").text(val);  //grabbing first div and displaying val 
+    //   });
+    startGame();
+})
